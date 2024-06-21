ef8fe90def292e1e1234dd43cf8ec6b821128e57
Akhil Manne <117744159+akhil-manne@users.noreply.github.com>
2024-06-21 10:46:37 +0530

    Add files via upload

---
+++ WinFormsApp2/MainForm.vb
@@ -0,0 +1,20 @@
+﻿Public Class MainForm
+    Private Sub btnViewSubmissions_Click(sender As Object, e As EventArgs) Handles btnViewSubmissions.Click
+        Dim viewForm As New ViewSubmissionsForm()
+        viewForm.Show()
+    End Sub
+
+    Private Sub btnCreateNewSubmission_Click(sender As Object, e As EventArgs) Handles btnCreateNewSubmission.Click
+        Dim createForm As New CreateSubmissionForm()
+        createForm.Show()
+    End Sub
+
+    Private Sub MainForm_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.Control AndAlso e.KeyCode = Keys.V Then
+            btnViewSubmissions.PerformClick()
+        ElseIf e.Control AndAlso e.KeyCode = Keys.N Then
+            btnCreateNewSubmission.PerformClick()
+        End If
+    End Sub
+End Class
+
---
+++ WinFormsApp2/ViewSubmissionsForm.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Net.Http
+Imports Newtonsoft.Json
+
+Public Class ViewSubmissionsForm
+    Private submissions As List(Of SubmissionDTO)
+    Private currentIndex As Integer = 0
+
+    Private Async Sub ViewSubmissionsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Await LoadSubmissions()
+        DisplaySubmission(currentIndex)
+    End Sub
+
+    Private Async Function LoadSubmissions() As Task
+        Using client As New HttpClient()
+            Try
+                Dim response As HttpResponseMessage = Await client.GetAsync("http://localhost:3000/read")
+                response.EnsureSuccessStatusCode()
+                Dim responseBody As String = Await response.Content.ReadAsStringAsync()
+                submissions = JsonConvert.DeserializeObject(Of List(Of SubmissionDTO))(responseBody)
+            Catch ex As Exception
+                MessageBox.Show($"Error loading submissions: {ex.Message}")
+                submissions = New List(Of SubmissionDTO)()
+            End Try
+        End Using
+    End Function
+
+    Private Sub DisplaySubmission(index As Integer)
+        If submissions IsNot Nothing AndAlso index >= 0 AndAlso index < submissions.Count Then
+            Dim submission As SubmissionDTO = submissions(index)
+            txtName.Text = submission.Name
+            txtEmail.Text = submission.Email
+            txtPhone.Text = submission.Phone
+            txtGitHubLink.Text = submission.GitHubLink
+            txtStopwatchTime.Text = submission.StopwatchTime
+        Else
+            ClearFields()
+        End If
+    End Sub
+
+    Private Sub ClearFields()
+        txtName.Text = ""
+        txtEmail.Text = ""
+        txtPhone.Text = ""
+        txtGitHubLink.Text = ""
+        txtStopwatchTime.Text = ""
+    End Sub
+
+    Private Sub btnPrevious_Click(sender As Object, e As EventArgs) Handles btnPrevious.Click
+        If currentIndex > 0 Then
+            currentIndex -= 1
+            DisplaySubmission(currentIndex)
+        End If
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        If currentIndex < submissions.Count - 1 Then
+            currentIndex += 1
+            DisplaySubmission(currentIndex)
+        End If
+    End Sub
+
+    Private Sub ViewSubmissionsForm_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.Control AndAlso e.KeyCode = Keys.P Then
+            btnPrevious.PerformClick()
+        ElseIf e.Control AndAlso e.KeyCode = Keys.N Then
+            btnNext.PerformClick()
+        End If
+    End Sub
+
+    ' Define a class to hold the submission data
+    Private Class SubmissionDTO
+        Public Property Name As String
+        Public Property Email As String
+        Public Property Phone As String
+        Public Property GitHubLink As String
+        Public Property StopwatchTime As String
+    End Class
+End Class
